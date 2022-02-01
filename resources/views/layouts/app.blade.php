5ae44d2ba98d1475e13032ff47ccb32e1e438783
Juan Esteban <juanes0202@gmail.com>
2022-02-01 15:16:09 -0500

    instalando tailwind css

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Movies</title>
+</head>
+
+<body>
+    <nav>New nav here</nav>
+    @yield('content')
+</body>
+
+</html>
