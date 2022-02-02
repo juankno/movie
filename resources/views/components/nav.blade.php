e5d516648c4bfd8019da66c5602a90415e3787fd
Juan Esteban <juanes0202@gmail.com>
2022-02-02 11:40:01 -0500

    terminando de ajustar estilos

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,5 +1,5 @@
 <nav class="border-b border-gray-800">
-    <div class="container mx-auto flex flex-col md:flex-row item-center justify-between px-4 py-6">
+    <div class="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between py-6">
         <ul class="flex flex-col md:flex-row items-center">
             <li>
                 <a href="/">
@@ -20,7 +20,7 @@
         </ul>
         <div class="flex flex-col md:flex-row items-center">
             <div class="relative mt-3 md:mt-0">
-                <input type="text" class="bg-gray-800 text-sm rounded-full w-64 pl-8 px-4 py-1 focus:outline-none focus:shadow-outline" placeholder="Search">
+                <input type="text" class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search">
                 <div class="absolute top-0">
                     <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24">
                         <path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z" />
