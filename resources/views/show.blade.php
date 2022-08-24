7b73ad3696c8cf50d085408e172d12734a830bfc
Juan Esteban <juanes0202@gmail.com>
2022-08-24 09:36:47 -0500

    cambiando dimensiones del boton

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -46,7 +46,7 @@
 
         @if (count($movie['videos']['results']) > 0)
         <div class="mt-12">
-            <a href="https://youtube.com/watch?v={{ $movie['videos']['results'][0]['key'] }}" target="_blank" class="flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
+            <a href="https://youtube.com/watch?v={{ $movie['videos']['results'][0]['key'] }}" target="_blank" class="max-w-xs w-40 flex items-center content-center  bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
                 <svg class="w-6 fill-current" viewBox="0 0 24 24">
                     <path d="M0 0h24v24H0z" fill="none" />
                     <path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
