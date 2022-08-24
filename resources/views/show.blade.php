d14bab9e73756e9549efcac27e36e5ae7bb4fdb8
Juan Esteban <juanes0202@gmail.com>
2022-08-24 11:29:01 -0500

    mostrando las imagenes en un modal

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -91,28 +91,31 @@
 
             @foreach ($movie['credits']['cast'] as $cast)
 
-            @if ($loop->index < 5) <div class="mt-8">
-                <a href="#">
-                    <img src="{{ 'https://image.tmdb.org/t/p/w300'. $cast['profile_path'] }}" alt="{{ $cast['name'] }}" class="hover:opacity-75 transition ease-in-out duration-150">
-                </a>
-                <div class="mt-2">
-                    <a href="#" class="text-lg mt-2 hover:text-gray:300">
-                        {{ $cast['name'] }}
-                    </a>
-                    <div class="text-sm text-gray-400">
-                        {{ $cast['character'] }}
+                @if ($loop->index < 5) 
+                    <div class="mt-8">
+                        <a 
+                            href="#">
+                            <img src="{{ 'https://image.tmdb.org/t/p/w300'. $cast['profile_path'] }}"
+                             alt="{{ $cast['name'] }}" 
+                             class="hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="mt-2">
+                            <a href="#" class="text-lg mt-2 hover:text-gray:300">
+                                {{ $cast['name'] }}
+                            </a>
+                            <div class="text-sm text-gray-400">
+                                {{ $cast['character'] }}
+                            </div>
+                        </div>
                     </div>
-                </div>
+                @endif
+
+            @endforeach
         </div>
-        @endif
-
-        @endforeach
-
     </div>
-</div>
 </div> <!-- end movie-cast -->
 
-<div class="movie-images">
+<div class="movie-images" x-data="{ isOpen: false, image: '' }">
     <div class="container mx-auto px-4 py-16">
         <h2 class="text-4xl font-semibold">Images</h2>
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
@@ -120,14 +123,35 @@
             @foreach ($movie['images']['backdrops'] as $image)
             @if ($loop->index < 9) 
             <div class="mt-8">
-                <a href="#">
+                <a href="#" @click.prevent="
+                 isOpen = true
+                 image= '{{ 'https://image.tmdb.org/t/p/original/'.$image['file_path'] }}'
+                 ">
                     <img src="{{ 'https://image.tmdb.org/t/p/w500'. $image['file_path'] }}" alt="{{ $image['vote_count'] }}" class="hover:opacity-75 transition ease-in-out duration-150">
                 </a>
             </div>
             @endif
             @endforeach
+            </div>
 
-    </div>
-</div>
-</div> <!-- end movie-images -->
+            <!-- Modal images -->
+            <div style="background-color: rgba(0, 0, 0, .5);" class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto" x-show.transition.opacity="isOpen" >
+                <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                    <div class="bg-gray-900 rounded">
+                        <div class="flex justify-end pr-4 pt-2">
+                            <button
+                                @click="isOpen = false"
+                                @keydown.escape.window="isOpen = false"
+                                class="text-3xl leading-none hover:text-gray-300">&times;
+                            </button>
+                        </div>
+                        <div class="modal-body px-8 py-8">
+                            <img :src="image" alt="poster">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div> <!-- end movie-images -->
 @endsection
